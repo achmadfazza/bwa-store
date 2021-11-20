9ce4a1db3782b1d14e4328c47a22ce2debba1ece
faza <arkadyfaza@gmail.com>
2021-11-20 19:46:59 +0700

    edit be detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -19,21 +19,28 @@ module.exports = {
       res.status(500).json({ message: err.message || 'Internal Server Error' });
     }
   },
+
   detailPage: async (req, res) => {
     try {
-      const { id } = req.params;
-
-      const voucher = await Voucher.findOne({ _id: id })
+      const voucher = await Voucher.findOne({ _id: req.params.id })
+        .populate('user', '_id name phone_number')
         .populate('category')
-        .populate('nominals')
-        .populate('user', '_id name phoneNumber');
+        .populate('nominals');
+
+      const payment = await Payment.find().populate('banks');
 
       if (!voucher) {
-        return res.status(404).json({ message: 'voucher game tidak ditemukan !' });
+        return res.status(404).json({ message: 'voucher game tidak ditemukan.!' });
       }
-      res.status(200).json({ data: voucher });
+
+      res.status(200).json({
+        data: {
+          detail: voucher,
+          payment,
+        },
+      });
     } catch (err) {
-      res.status(500).json({ message: err.message || 'Internal Server Error' });
+      res.status(500).json({ message: err.message || 'Internal server error' });
     }
   },
   category: async (req, res) => {
